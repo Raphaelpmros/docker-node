05a78288e74ec48ba354308597969e50e3bea6d4
Raphael Mello <raphaelpmros@hotmail.com>
2024-02-09 18:05:42 -0300

    começando funcionalidade de inserir dados na tabela

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ con.connect(function (err) {
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({extended: true}))
 
 const allProductsPromise = new Promise((resolve, reject) => {
   con.query("SELECT * FROM products", function (err, result, fields) {
@@ -35,7 +35,6 @@ const allProductsPromise = new Promise((resolve, reject) => {
   });
 });
 
-
 app.get("/products", async (req, res) => {
   try {
     const products = await allProductsPromise;
@@ -43,9 +42,42 @@ app.get("/products", async (req, res) => {
     res.render("products", { products });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: err });
   }
 });
+
+app.get("/products/new", (req, res) => {
+  res.render("new");
+})
+
+app.post('/products', (req, res) => {
+  const newProduct = new Product(req.body)
+  res.send('making your product')
+})
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    con.query(`SELECT * FROM products WHERE id = '${id}'`, function (err, result, fields) {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      if (result.length === 0) {
+        res.status(404).send('Product not found');
+        return;
+      }
+      const product = result[0];
+      console.log(product)
+      res.render('show', { product });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
