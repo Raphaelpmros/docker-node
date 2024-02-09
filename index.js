8409fb09efc70f4447cbd21790e9787218efa7f8
Raphael Mello <raphaelpmros@hotmail.com>
2024-02-09 14:42:55 -0300

    Primeira tela criada de forma basica para ver se o select do banco está funcionando

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mysql = require('mysql2');
-
+const path = require("path");
+const mysql = require("mysql2");
 
 const con = mysql.createConnection({
-  host: 'mysql-db',
-  user: 'root',
-  password: 'root',
-  database: 'products',
+  host: "mysql-db",
+  user: "root",
+  password: "root",
+  database: "products",
 });
 
-
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");
+  return (allProducts = con.query(
+    "SELECT * FROM products",
+    function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    }
+  ));
 });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get('/dog', (req, res) => {
-  res.send("WOLF")
-})
 
-app.get('products' (req, res) => {
+const allProductsPromise = new Promise((resolve, reject) => {
+  con.query("SELECT * FROM products", function (err, result, fields) {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
 
-})
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await allProductsPromise;
+    console.log(products);
+    res.render("products", { products });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err });
+  }
+});
 
 app.listen(3000, () => {
-  console.log("listening on port 3000")
-})
+  console.log("listening on port 3000");
+});
